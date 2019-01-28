a305a322685a5a5bae2b52f64cd544f8a2a8d122
Pornthep Nivatyakul <kaebmoo@yahoo.com>
2019-01-28 09:54:21 +0700

    Update sdm_test_serial.ino
    send to thingsboard

--- sdm_test_serial/sdm_test_serial.ino
+++ sdm_test_serial/sdm_test_serial.ino
@@ -7,20 +7,27 @@
 
 
 #include <SDM.h>
-// #include <ESP8266WiFi.h>          //https://github.com/esp8266/Arduino
-#include <WiFi.h> // esp32
+#include <ESP8266WiFi.h>          //https://github.com/esp8266/Arduino
 #include <ThingSpeak.h>
-// #include <BlynkSimpleEsp8266.h>
+#include <BlynkSimpleEsp8266.h>
 #include <WiFiClient.h>
-#include <BlynkSimpleEsp32.h>
 
+// #include <BlynkSimpleEsp32.h>
+// #include <WiFi.h> // esp32
+
+#include <ArduinoJson.h>
+#include <PubSubClient.h>
+
+#define THINGSBOARD
+#define WIFI_AP "Red"
+#define WIFI_PASSWORD "12345678"
 
 // SDM<9600> sdm;
 SDM sdm(Serial, 9600, NOT_A_PIN, SERIAL_8N1, false);
 
 // ThingSpeak information
 char thingSpeakAddress[] = "api.thingspeak.com";
-unsigned long channelID = 360709;
+unsigned long channelID = 585496;
 char *readAPIKey = "5S8UD732WGE1UVO7";
 char *writeAPIKey = "D0T6TVCX48SBB9U5";
 
@@ -33,15 +40,32 @@ float tmpAmp = NAN;
 float tmpWat = NAN;
 float tmpFre = NAN;
 float tmpEne = NAN;
+float tmpPf = NAN;
+float tmpMaxPowerDemand = NAN;
 int readCount = 0;
 float voltage = 0.0;
 float amp = 0.0;
 float watt = 0.0;
 float freq = 0.0;
 float energy = 0.0;
+float pf = 0.0;
+float MaxPowerDemand = 0.0;
 
-int BUILTIN_LED = 2;  // ESP32 board
+// int BUILTIN_LED = 2;  // ESP32 board
 
+
+char thingsboardServer[40] = "box.greenwing.email";
+int  mqttport = 1883;                      // 1883 or 1888
+char token[32] = "bLdQmUD1GvlIsCzirDld";   // device token from thingsboard server
+
+char *mqtt_user = "seal";
+char *mqtt_password = "sealwiththekiss";
+const char* mqtt_server = "db.ogonan.com";
+int mqtt_reconnect = 0;
+const int MAXRETRY=5;
+char *myRoom = "smartmeter/1";
+
+PubSubClient mqttClient(client);
 
 void setup() {
   pinMode(BUILTIN_LED, OUTPUT);
@@ -50,7 +74,7 @@ void setup() {
   
   sdm.begin();
   
-  WiFi.begin("Red1", "12345678");
+  WiFi.begin(WIFI_AP, WIFI_PASSWORD);
   Serial.print("Connecting");
   Serial.println();
 
@@ -68,13 +92,21 @@ void setup() {
   Serial.println(WiFi.localIP()); 
   
   ThingSpeak.begin( client );
-  // blynkTimer.setInterval(60000L, sendThingSpeak);                   // send data to thingspeak
+  setupMqtt();
+  blynkTimer.setInterval(60000L, sendThingSpeak);                   // send data to thingspeak
   blynkTimer.setInterval(5000L, readMeterData);
+  blynkTimer.setInterval(15000L, sendThingsBoard);
 }
 
 void loop() {
   blynkTimer.run();
-  
+  #ifdef THINGSBOARD
+  if ( !mqttClient.connected() ) {
+    reconnect();
+  }
+
+  mqttClient.loop();
+  #endif
 }
 
 void readMeterData()
@@ -88,6 +120,10 @@ void readMeterData()
   tmpFre = sdm.readVal(SDM230_FREQUENCY);
   delay(50);
   tmpEne = sdm.readVal(SDM230_TOTAL_ACTIVE_ENERGY);
+  delay(50);
+  tmpPf = sdm.readVal(SDM230_POWER_FACTOR);
+  delay(50);
+  tmpMaxPowerDemand = sdm.readVal(SDM230_MAXIMUM_SYSTEM_POWER_DEMAND);
 
   if (!isnan(tmpVol)) {
     digitalWrite(BUILTIN_LED, LOW);
@@ -111,16 +147,27 @@ void readMeterData()
     Serial.print("Total Energy: ");
     Serial.print(tmpEne);
     Serial.println("kWh");
+
+    Serial.print("Power Factor: ");
+    Serial.print(tmpPf);
+    Serial.println("");
+
+    Serial.print("Maximum System Power Demand: ");
+    Serial.print(tmpMaxPowerDemand);
+    Serial.println("W");
+
     voltage += tmpVol;
     amp += tmpAmp;
     watt += tmpWat;
     freq = tmpFre;
     energy = tmpEne;
+    pf = tmpPf;
+    MaxPowerDemand = tmpMaxPowerDemand;
     readCount++;
   }
-  delay(500);
+  delay(100);
   digitalWrite(BUILTIN_LED, HIGH);
-  delay(500);
+  delay(100);
   digitalWrite(BUILTIN_LED, LOW);
 }
 
@@ -133,7 +180,7 @@ void sendThingSpeak()
     watt = watt / readCount;    
   }
 
-    Serial.println();
+    Serial.println("Sending data to thingspeak");
     Serial.print("Voltage:   ");
     Serial.print(voltage);
     Serial.println("V");
@@ -171,4 +218,186 @@ void sendThingSpeak()
     amp = 0;
     watt = 0;
     readCount = 0;
+}
+
+void sendThingsBoard()
+{
+  readMeterData();
+  if (readCount != 0) {    
+    // Just debug messages
+    Serial.print( "Sending data to thingsboard: [" );
+    
+    Serial.print( "]   -> " );
+  
+    // Prepare a JSON payload string
+    String payload = "{";
+    payload += "\"Volt\":"; payload += voltage / readCount; payload += ",";
+    payload += "\"Current\":"; payload += amp / readCount; payload += ",";
+    payload += "\"Watt\":"; payload += watt / readCount; payload += ",";
+    // payload += "\"Frequency\":"; payload += freq; payload += ",";
+    payload += "\"Energy\":"; payload += energy; payload += ",";
+    payload += "\"PF\":"; payload += pf; payload += ",";
+    payload += "\"Max Power Demand\":"; payload += MaxPowerDemand; 
+    payload += "}";
+  
+    // Send payload
+    char attributes[100];
+    payload.toCharArray( attributes, 100 );
+    mqttClient.publish( "v1/devices/me/telemetry", attributes );
+    Serial.println( attributes );
+    Serial.println();
+    delay(100);
+    sendStatus();
+  }
+}
+
+void sendStatus()
+{
+  String payload = "{";
+  if (amp > 0.0) {
+    payload += "\"Active\":"; payload += true;   
+  }
+  else if (amp == 0.0) {
+    payload += "\"Active\":"; payload += false;   
+  }
+  
+  payload += "}";
+
+  // Send payload
+  char attributes[100];
+  payload.toCharArray( attributes, 100 );
+  mqttClient.publish( "v1/devices/me/telemetry", attributes );
+  Serial.println( attributes );
+  Serial.println();
+}
+
+void setupMqtt()
+{
+  #ifdef THINGSBOARD
+  Serial.println("FARM Server : "+String(thingsboardServer)+" mqtt port: "+String(mqttport));
+  mqttClient.setServer(thingsboardServer, mqttport);  // default port 1883, mqtt_server, thingsboardServer
+  #else
+  mqttClient.setServer(mqtt_server, mqttport);
+  #endif
+  mqttClient.setCallback(callback);
+  if (mqttClient.connect(myRoom, token, NULL)) {
+    Serial.print("mqtt connected : ");
+    #ifdef THINGSBOARD
+    Serial.println(thingsboardServer);  // mqtt_server
+    #else
+    Serial.println(mqtt_server);  // mqtt_server
+    #endif
+    
+    Serial.println();
+  }
+}
+
+
+void reconnect()
+{
+  // Loop until we're reconnected
+  int status = WL_IDLE_STATUS;
+
+  while (!mqttClient.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    status = WiFi.status();
+    if ( status != WL_CONNECTED) {
+      WiFi.begin(WIFI_AP, WIFI_PASSWORD);
+      while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+      }
+      Serial.println("Connected to AP");
+    }
+    // Attempt to connect
+    #ifdef THINGSBOARD
+    if (mqttClient.connect(myRoom, token, NULL)) {  // connect to thingsboards
+    #else
+    if (mqttClient.connect(myRoom, mqtt_user, mqtt_password)) {  // connect to thingsboards
+    #endif
+      Serial.print("connected : ");
+      Serial.println(thingsboardServer); // mqtt_server
+
+
+
+      // Once connected, publish an announcement...
+      // mqttClient.publish(roomStatus, "hello world");
+
+    } else {
+      Serial.print("failed, reconnecting state = ");
+      Serial.print(mqttClient.state());
+      Serial.print(" try : ");
+      Serial.print(mqtt_reconnect+1);
+      Serial.println(" try again in 2 seconds");
+      // Wait 2 seconds before retrying
+      delay(2000);
+    }
+    mqtt_reconnect++;
+    if (mqtt_reconnect > MAXRETRY) {
+      mqtt_reconnect = 0;
+      break;
+    }
+  }
+}
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  int i;
+
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  for (i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
+
+  char json[length + 1];
+  strncpy (json, (char*)payload, length);
+  json[length] = '\0';
+
+  // Decode JSON request
+  StaticJsonBuffer<200> jsonBuffer;
+  JsonObject& data = jsonBuffer.parseObject((char*)json);
+
+  if (!data.success())
+  {
+    Serial.println("parseObject() failed");
+    return;
+  }
+
+  // Check request method
+  String methodName = String((const char*)data["method"]);
+
+  if (methodName.equals("checkStatus")) {
+    String responseTopic = String(topic);
+    responseTopic.replace("request", "response");
+    mqttClient.publish(responseTopic.c_str(), getStatus().c_str());
+  } 
+  else if (methodName.equals("setGpioStatus")) {
+
+  }
+
+}
+
+String getStatus()
+{
+  // Prepare JSON payload string
+  StaticJsonBuffer<200> jsonBuffer;
+  JsonObject& data = jsonBuffer.createObject();
+
+  if (amp > 0.0) {
+    data["Active"] = true;
+  }
+  else if (amp == 0.0) {
+    data["Active"] = false;
+  }
+  
+  char payload[256];
+  data.printTo(payload, sizeof(payload));
+
+  String strPayload = String(payload);
+  Serial.print("Get pump status: ");
+  Serial.println(strPayload);
+
+  return strPayload;
 }
