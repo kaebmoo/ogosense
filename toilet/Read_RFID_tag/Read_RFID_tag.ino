79174a9e3066022725147db7197b380443c9557e
Pornthep Nivatyakul <kaebmoo@yahoo.com>
2019-09-13 19:25:03 +0700

    Update Read_RFID_tag.ino

--- toilet/Read_RFID_tag/Read_RFID_tag.ino
+++ toilet/Read_RFID_tag/Read_RFID_tag.ino
@@ -12,11 +12,25 @@
  */
 
 #include <WiFiManager.h>         //https://github.com/tzapu/WiFiManager
+#include <PubSubClient.h>
+#include <ArduinoJson.h> // https://arduinojson.org/v5/assistant/
 
 #include "MFRC522.h"
 #define RST_PIN  D1 // RST-PIN for RC522 - RFID - SPI - Modul GPIO5 
 #define SS_PIN  D2 // SDA-PIN for RC522 - RFID - SPI - Modul GPIO4 
 MFRC522 mfrc522(SS_PIN, RST_PIN); // Create MFRC522 instance
+
+unsigned long channelID = 793986;
+const char* server = "mqtt.ogonan.com"; 
+char mqttUserName[] = "kaebmoo";  // Can be any name.
+char mqttPass[] = "sealwiththekiss";  // Change this your MQTT API Key from Account > MyProfile.
+String subscribeTopic = "channels/" + String( channelID ) + "/subscribe/fields/field3";
+WiFiClient client; 
+PubSubClient mqttClient(client); 
+static const char alphanum[] ="0123456789"
+                              "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+                              "abcdefghijklmnopqrstuvwxyz";  // For random generation of client ID.
+
 
 void setup() {
   // put your setup code here, to run once:
@@ -24,6 +38,7 @@ void setup() {
   SPI.begin();           // Init SPI bus
   mfrc522.PCD_Init();    // Init MFRC522
   wifiConnect();
+  mqttConnect();
 }
 
 void loop() {
@@ -94,4 +109,70 @@ void ondemandWiFi()
     //if you get here you have connected to the WiFi
     Serial.println("connected...yeey :)");
     
+}
+
+
+void mqttConnect()
+{
+  mqttClient.setServer(server, 1883);   // Set the MQTT broker details.
+  mqttClient.setCallback(callback);  
+}
+
+void reconnect() 
+{
+  char clientID[9];
+
+  // Loop until reconnected.
+  while (!mqttClient.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Generate ClientID
+    for (int i = 0; i < 8; i++) {
+        clientID[i] = alphanum[random(51)];
+    }
+    clientID[8]='\0';
+    // Connect to the MQTT broker
+    if (mqttClient.connect(clientID,mqttUserName,mqttPass)) {
+      Serial.print("Connected with Client ID:  ");
+      Serial.print(String(clientID));
+      Serial.print(", Username: ");
+      Serial.print(mqttUserName);
+      Serial.print(" , Passwword: ");
+      Serial.println(mqttPass);
+      mqttClient.subscribe( subscribeTopic.c_str() );
+    } 
+    else {
+      Serial.print("failed, rc=");
+      // Print to know why the connection failed.
+      // See https://pubsubclient.knolleary.net/api.html#state for the failure code explanation.
+      Serial.print(mqttClient.state());
+      Serial.println(" try again in 5 seconds");
+      delay(5000);
+    }
+  }
+}
+
+void callback(char* topic, byte* payload, unsigned int length) 
+{
+  int i;
+  char p[length + 1];
+  memcpy(p, payload, length);
+  p[length] = NULL;
+
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  for (i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
+
+  /*
+  if (!strncmp(p, "on", 2) || !strncmp(p, "1", 1)) {
+    digitalWrite(RELAY1, HIGH);
+    idTimerSwitch = timerSwitch.after(10000, turnOn);
+  }
+  else if (!strncmp(p, "off", 3) || !strncmp(p, "0", 1) ) {
+    digitalWrite(RELAY1, LOW);
+  }
+  */
 }
